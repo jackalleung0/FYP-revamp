b22b617bf6d5fdacbadb3f333be0681d99296f85
jackalleung0 <jackalleung0@gmail.com>
2022-03-31 15:56:24 +0000

    extract component

---
+++ src/BackIcon.tsx
@@ -0,0 +1,20 @@
+export function BackIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19.214"
+      height="16.828"
+      viewBox="0 0 19.214 16.828"
+    >
+      <path
+        id="Back_Icon"
+        d="M13,21,6,14m0,0,7-7M6,14H22.8"
+        transform="translate(-5.086 -6.086)"
+        fill="none"
+        stroke="#000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2" />
+    </svg>
+  );
+}
