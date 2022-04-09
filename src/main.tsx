243e6a6f01f3b635d1186c7c037c32c3d1448700
jackalleung0 <jackalleung0@gmail.com>
2022-04-09 23:10:20 +0800

    remove focus ring

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ function Root() {
         // Override any other properties from default theme
         fontFamily: "Open Sans, sans serif",
         spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+        focusRing: "never",
       }}
     >
       <Rout />
